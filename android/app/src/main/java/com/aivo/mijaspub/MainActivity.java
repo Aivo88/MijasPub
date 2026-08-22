727ea359604ecf45cb8cd92c7eb15fd6c637540e
Aivo88 <antti.kaariainen88@gmail.com>
2026-08-22 16:25:59 +0200

    Add files via upload

--- android/app/src/main/java/com/aivo/mijaspub/MainActivity.java
+++ android/app/src/main/java/com/aivo/mijaspub/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
@@ -99,6 +100,26 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             web.loadUrl(START_URL);
         }
+
+        hideSystemUI();
+    }
+
+    // Kioskitila: piilota Androidin ala- ja ylapalkit (back/home/recents).
+    private void hideSystemUI() {
+        View decor = getWindow().getDecorView();
+        decor.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) hideSystemUI();
     }
 
     @Override
