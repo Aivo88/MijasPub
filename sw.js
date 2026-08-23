f449317fdc9b784f37400611f203b3bcd291cb7f
Aivo88 <antti.kaariainen88@gmail.com>
2026-08-23 09:31:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SE7EN V2 service worker
 // Bump CACHE on every release.
-const CACHE = 'mijaspub-v23';
+const CACHE = 'mijaspub-v24';
 const ASSETS = ['./', './index.html', './manifest.json', './header-default.png',
                 './icon-192.png', './icon-512.png', './bg.jpg'];
 
